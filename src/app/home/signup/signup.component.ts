1af094bd8f4ef925d51614471c2206f0881994ad
Akash <akasharul2101@gmail.com>
2020-09-24 12:03:51 +0530

    Add some features to forms
    login and signup form has signup & login link at the bottom.
    confirm password validations and other basic validations are added and
    error messages are added to the signup form.

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms'
+import { FormControl, FormGroup, ValidatorFn, Validators, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,18 +7,28 @@ import { FormBuilder } from '@angular/forms'
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  signupform = this.fb.group({
-    username: [''],
-    password: ['']
-  });
+  public signupform: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor() { }
 
   ngOnInit(): void {
+    this.signupform = new FormGroup({
+      username: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      confpassword: new FormControl('',[Validators.required, Validators.minLength(3)])
+    }, {validators: this.confirmPassword})
   }
 
   onSubmit() {
-    console.log('signup form submitted');
+    alert('signup form submitted');
   }
+
+  confirmPassword: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+    const firstPassword = control.get('password');
+    const secondPassword = control.get('confpassword');
+    //alert(firstPassword.value + ' ' + secondPassword.value)
+  
+    return firstPassword && secondPassword && firstPassword.value != secondPassword.value ? {passwordMisMatch: true} : null;
+  };
 
 }
